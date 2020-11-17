a9f42fb092027176997f8a40cab4fd4cfcc232f2
radzikkos <lukaszrad99@wp.pl>
2020-11-17 01:33:48 +0100

    Added local

--- lab04/main.py
+++ lab04/main.py
@@ -21,7 +21,7 @@ class Relaxation:
         self.sigmaY = 0.1 * self.yMax
 
         #self.ro = np.zeros((self.nx, self.ny))
-
+        self.omegaL = [1.0, 1.4, 1.8, 1.9]
     def ro1(self, x, y):
         part1 = - math.pow((x - 0.35 * self.xMax), 2) / math.pow(self.sigmaX, 2)
         part2 = - math.pow((y - 0.5 * self.yMax),2) / math.pow(self.sigmaY, 2)
@@ -108,7 +108,7 @@ class Relaxation:
         for i in range(self.nx + 1):
             for j in range(self.ny + 1):
                 map = np.vstack([map, np.array([[self.delta * i, self.delta * j,vOld[i,j]]])])
-                print(i * j)
+                #print(i * j)
                 if (i > 0 and j > 0 and i < self.nx and j < self.ny):
                     error = np.vstack([error, np.array([[self.delta * i, self.delta * j, self.err(vOld, i, j)]])])
                 else:
@@ -124,6 +124,57 @@ class Relaxation:
         fig.colorbar(surf)
 
         plt.show()
-Relaxation().global_relaxation()
+
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        surf = ax.plot_surface(error[1:, 0], error[1:, 1], error[1:2], rstride=1, cstride=1, cmap=cm.jet, extend3d=True)
+        ax.set_zlim3d(-1.01, 1.01)
+
+        ax.w_zaxis.set_major_locator(LinearLocator(10))
+        ax.w_zaxis.set_major_formatter(FormatStrFormatter('%.03f'))
+
+        fig.colorbar(surf)
+
+        plt.show()
+
+    def local_relaxation_core(self, omega, v, ro):
+        for i in range(self.nx):
+            for j in range(self.ny):
+                v[i,j] = (1 - omega) * v[i,j] + (omega / 4.0) * (v[i + 1,j] + v[i - 1,j] + v[i,j + 1] + v[i,j - 1] + math.pow(self.delta, 2) / self.epsilon * ro[i,j]);
+
+        for j in range(self.ny):
+            v[0,j] = v[1,j]
+            v[self.nx, j] = v[self.nx - 1, j]
+        return v
+    def local_relaxation(self):
+        for omega in self.omegaL:
+            integral = np.zeros((1,2))  # to plot
+            sIt = np.zeros((1, 2))
+            v = np.zeros((self.nx + 1, self.ny + 1))
+            v = self.edge_cases(v)
+            ro = self.fill_ro()
+            sIt[0,1] = self.stop_condition(v, ro)
+
+            it = 0
+
+            while True:
+                it += 1
+                self.local_relaxation_core(omega, v, ro)
+                sIt[0,0] = sIt[0,1]
+                sIt[0,1] = self.stop_condition(v, ro)
+
+                integral = np.vstack([integral, np.array([[it, sIt[0,0]]])])
+
+                if (math.fabs((sIt[0, 0] - sIt[0, 1]) / sIt[0, 0]) < self.TOL):
+                    break
+            plt.plot(integral[1:, 0], integral[1:, 1], label=(str)(omega))
+            # print(vOld)
+        plt.title("Integral")
+        plt.xlabel("it")
+        plt.ylabel("s(it)")
+        plt.legend()
+        plt.show()
+#Relaxation().global_relaxation()
+Relaxation().local_relaxation()
 
 
